55973a5f967acb9d8f14b89a5b55830a60956357
HANCKERS <cocshubh49@gmail.com>
2023-12-22 22:58:53 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const videos = [{
+    id:1,
+    title: 'Movie review',
+    views: '999K',
+    time: '1 year ago',
+    channel: 'shubham',
+    verified: true
+  },{
+    id:2,  
+    title: 'Web series review',
+    views: '100K',
+    time: '1 year ago',
+    channel: 'shubham',
+    verified: false
+  },
+  { id:3,
+    title: 'Anime review',
+    views: '1M',
+    time: '1 month ago',
+    channel: 'shubham',
+    verified: true
+  }];
+
+export default videos;
---
+++ src/data/moredata.js
@@ -0,0 +1,684 @@
+
+const videos = [
+   {
+  "id": 4,
+  "channel": "shubham",
+  "views": "89M",
+  "title": "Olympia Part One: Festival of the Nations (Olympia 1. Teil - Fest der Völker)",
+  "time": "15 days ago",
+  "verified": false
+}, {
+  "id": 5,
+  "channel": "shubham",
+  "views": "89M",
+  "title": "Bosko's Parlor Pranks",
+  "time": "76 days ago",
+  "verified": false
+}, {
+  "id": 6,
+  "channel": "shubham",
+  "views": "96M",
+  "title": "Giant",
+  "time": "47 days ago",
+  "verified": true
+}, {
+  "id": 7,
+  "channel": "shubham",
+  "views": "60M",
+  "title": "Repentance (Monanieba)",
+  "time": "77 days ago",
+  "verified": true
+}, {
+  "id": 8,
+  "channel": "shubham",
+  "views": "25M",
+  "title": "Down to the Bone",
+  "time": "69 days ago",
+  "verified": false
+}, {
+  "id": 9,
+  "channel": "shubham",
+  "views": "58M",
+  "title": "Chinese Take-Out (Chinese Take-Away) (Un cuento chino)",
+  "time": "71 days ago",
+  "verified": true
+}, {
+  "id": 10,
+  "channel": "shubham",
+  "views": "79M",
+  "title": "Brass Teapot, The",
+  "time": "14 days ago",
+  "verified": true
+}, {
+  "id": 11,
+  "channel": "shubham",
+  "views": "8M",
+  "title": "Queen Kelly",
+  "time": "89 days ago",
+  "verified": false
+}, {
+  "id": 12,
+  "channel": "shubham",
+  "views": "86M",
+  "title": "D3: The Mighty Ducks",
+  "time": "56 days ago",
+  "verified": false
+}, {
+  "id": 13,
+  "channel": "shubham",
+  "views": "1M",
+  "title": "Wild Gals Of The Naked West",
+  "time": "47 days ago",
+  "verified": false
+}, {
+  "id": 14,
+  "channel": "shubham",
+  "views": "62M",
+  "title": "Pleasantville",
+  "time": "8 days ago",
+  "verified": false
+}, {
+  "id": 15,
+  "channel": "shubham",
+  "views": "16M",
+  "title": "Hypothesis of the Stolen Painting, The (L'hypothèse du tableau volé)",
+  "time": "94 days ago",
+  "verified": true
+}, {
+  "id": 16,
+  "channel": "shubham",
+  "views": "68M",
+  "title": "Beyond the Black Rainbow",
+  "time": "39 days ago",
+  "verified": true
+}, {
+  "id": 17,
+  "channel": "shubham",
+  "views": "43M",
+  "title": "Varg Veum - Fallen Angels (Varg Veum - Falne Engler)",
+  "time": "56 days ago",
+  "verified": false
+}, {
+  "id": 18,
+  "channel": "shubham",
+  "views": "15M",
+  "title": "The Horribly Slow Murderer with the Extremely Inefficient Weapon",
+  "time": "81 days ago",
+  "verified": true
+}, {
+  "id": 19,
+  "channel": "shubham",
+  "views": "38M",
+  "title": "I Am Number Four",
+  "time": "31 days ago",
+  "verified": false
+}, {
+  "id": 20,
+  "channel": "shubham",
+  "views": "7M",
+  "title": "Before Night Falls",
+  "time": "92 days ago",
+  "verified": false
+}, {
+  "id": 21,
+  "channel": "shubham",
+  "views": "91M",
+  "title": "Across the Bridge",
+  "time": "90 days ago",
+  "verified": true
+}, {
+  "id": 22,
+  "channel": "shubham",
+  "views": "98M",
+  "title": "Randy and the Mob",
+  "time": "76 days ago",
+  "verified": true
+}, {
+  "id": 23,
+  "channel": "shubham",
+  "views": "76M",
+  "title": "Little Ashes",
+  "time": "48 days ago",
+  "verified": true
+}, {
+  "id": 24,
+  "channel": "shubham",
+  "views": "95M",
+  "title": "Green Zone",
+  "time": "38 days ago",
+  "verified": true
+}, {
+  "id": 25,
+  "channel": "shubham",
+  "views": "76M",
+  "title": "Tartuffe (Herr Tartüff)",
+  "time": "88 days ago",
+  "verified": false
+}, {
+  "id": 26,
+  "channel": "shubham",
+  "views": "56M",
+  "title": "Killer Holiday",
+  "time": "82 days ago",
+  "verified": true
+}, {
+  "id": 27,
+  "channel": "shubham",
+  "views": "44M",
+  "title": "Comic-Con Episode IV: A Fan's Hope",
+  "time": "4 days ago",
+  "verified": true
+}, {
+  "id": 28,
+  "channel": "shubham",
+  "views": "30M",
+  "title": "What Have You Done to Solange?",
+  "time": "27 days ago",
+  "verified": false
+}, {
+  "id": 29,
+  "channel": "shubham",
+  "views": "45M",
+  "title": "Songwriter",
+  "time": "15 days ago",
+  "verified": true
+}, {
+  "id": 30,
+  "channel": "shubham",
+  "views": "74M",
+  "title": "Stars and Stripes Forever",
+  "time": "85 days ago",
+  "verified": false
+}, {
+  "id": 31,
+  "channel": "shubham",
+  "views": "64M",
+  "title": "Killer Joe",
+  "time": "61 days ago",
+  "verified": true
+}, {
+  "id": 32,
+  "channel": "shubham",
+  "views": "32M",
+  "title": "Partners: The Movie II (Aibô: Gekijô-ban II)",
+  "time": "68 days ago",
+  "verified": true
+}, {
+  "id": 33,
+  "channel": "shubham",
+  "views": "70M",
+  "title": "Blood Done Sign My Name",
+  "time": "29 days ago",
+  "verified": false
+}, {
+  "id": 34,
+  "channel": "shubham",
+  "views": "35M",
+  "title": "Friday Night Lights",
+  "time": "25 days ago",
+  "verified": true
+}, {
+  "id": 35,
+  "channel": "shubham",
+  "views": "33M",
+  "title": "City of No Limits, The (la ciudad sin límites, En)",
+  "time": "54 days ago",
+  "verified": false
+}, {
+  "id": 36,
+  "channel": "shubham",
+  "views": "71M",
+  "title": "8 1/2 (8½)",
+  "time": "3 days ago",
+  "verified": false
+}, {
+  "id": 37,
+  "channel": "shubham",
+  "views": "61M",
+  "title": "Keeper, The",
+  "time": "56 days ago",
+  "verified": false
+}, {
+  "id": 38,
+  "channel": "shubham",
+  "views": "76M",
+  "title": "Kwaidan (Kaidan)",
+  "time": "36 days ago",
+  "verified": false
+}, {
+  "id": 39,
+  "channel": "shubham",
+  "views": "39M",
+  "title": "Bringing Out the Dead",
+  "time": "75 days ago",
+  "verified": false
+}, {
+  "id": 40,
+  "channel": "shubham",
+  "views": "80M",
+  "title": "Young One, The",
+  "time": "35 days ago",
+  "verified": true
+}, {
+  "id": 41,
+  "channel": "shubham",
+  "views": "25M",
+  "title": "Zoot Suit",
+  "time": "45 days ago",
+  "verified": true
+}, {
+  "id": 42,
+  "channel": "shubham",
+  "views": "88M",
+  "title": "Gold",
+  "time": "28 days ago",
+  "verified": true
+}, {
+  "id": 43,
+  "channel": "shubham",
+  "views": "18M",
+  "title": "Doing Time on Maple Drive",
+  "time": "41 days ago",
+  "verified": true
+}, {
+  "id": 44,
+  "channel": "shubham",
+  "views": "44M",
+  "title": "Life On A String (Bian chang Bian Zou)",
+  "time": "86 days ago",
+  "verified": false
+}, {
+  "id": 45,
+  "channel": "shubham",
+  "views": "34M",
+  "title": "La Morte Rouge",
+  "time": "100 days ago",
+  "verified": true
+}, {
+  "id": 46,
+  "channel": "shubham",
+  "views": "86M",
+  "title": "Lizard, The (Marmoulak)",
+  "time": "3 days ago",
+  "verified": false
+}, {
+  "id": 47,
+  "channel": "shubham",
+  "views": "49M",
+  "title": "Red River",
+  "time": "76 days ago",
+  "verified": true
+}, {
+  "id": 48,
+  "channel": "shubham",
+  "views": "75M",
+  "title": "Television Under the Swastika (Das Fernsehen unter dem Hakenkreuz)",
+  "time": "9 days ago",
+  "verified": true
+}, {
+  "id": 49,
+  "channel": "shubham",
+  "views": "2M",
+  "title": "Hamlet (Gamlet)",
+  "time": "42 days ago",
+  "verified": true
+}, {
+  "id": 50,
+  "channel": "shubham",
+  "views": "85M",
+  "title": "Tour De Force (Hin und weg)",
+  "time": "52 days ago",
+  "verified": true
+}, {
+  "id": 51,
+  "channel": "shubham",
+  "views": "86M",
+  "title": "Lucky Ones, The",
+  "time": "93 days ago",
+  "verified": true
+}, {
+  "id": 52,
+  "channel": "shubham",
+  "views": "46M",
+  "title": "Black or White",
+  "time": "69 days ago",
+  "verified": false
+}, {
+  "id": 53,
+  "channel": "shubham",
+  "views": "36M",
+  "title": "Jab We Met",
+  "time": "49 days ago",
+  "verified": false
+}, {
+  "id": 54,
+  "channel": "shubham",
+  "views": "3M",
+  "title": "Twelve Angry Men",
+  "time": "21 days ago",
+  "verified": false
+}, {
+  "id": 55,
+  "channel": "shubham",
+  "views": "10M",
+  "title": "Passion Fish",
+  "time": "40 days ago",
+  "verified": false
+}, {
+  "id": 56,
+  "channel": "shubham",
+  "views": "64M",
+  "title": "Halo Legends",
+  "time": "85 days ago",
+  "verified": false
+}, {
+  "id": 57,
+  "channel": "shubham",
+  "views": "26M",
+  "title": "She Wore a Yellow Ribbon",
+  "time": "31 days ago",
+  "verified": false
+}, {
+  "id": 58,
+  "channel": "shubham",
+  "views": "5M",
+  "title": "Crazy Thunder Road",
+  "time": "83 days ago",
+  "verified": false
+}, {
+  "id": 59,
+  "channel": "shubham",
+  "views": "45M",
+  "title": "The Broken Jug",
+  "time": "50 days ago",
+  "verified": false
+}, {
+  "id": 60,
+  "channel": "shubham",
+  "views": "44M",
+  "title": "Action in the North Atlantic",
+  "time": "100 days ago",
+  "verified": false
+}, {
+  "id": 61,
+  "channel": "shubham",
+  "views": "94M",
+  "title": "The Search for Santa Paws",
+  "time": "12 days ago",
+  "verified": true
+}, {
+  "id": 62,
+  "channel": "shubham",
+  "views": "54M",
+  "title": "Futurama: Into the Wild Green Yonder",
+  "time": "27 days ago",
+  "verified": false
+}, {
+  "id": 63,
+  "channel": "shubham",
+  "views": "19M",
+  "title": "Wolfsburg",
+  "time": "65 days ago",
+  "verified": true
+}, {
+  "id": 64,
+  "channel": "shubham",
+  "views": "3M",
+  "title": "Brenda Starr",
+  "time": "23 days ago",
+  "verified": true
+}, {
+  "id": 65,
+  "channel": "shubham",
+  "views": "34M",
+  "title": "Crawling Hand, The",
+  "time": "84 days ago",
+  "verified": false
+}, {
+  "id": 66,
+  "channel": "shubham",
+  "views": "71M",
+  "title": "God Grew Tired of Us",
+  "time": "50 days ago",
+  "verified": false
+}, {
+  "id": 67,
+  "channel": "shubham",
+  "views": "72M",
+  "title": "Aviator's Wife, The (La femme de l'aviateur)",
+  "time": "97 days ago",
+  "verified": true
+}, {
+  "id": 68,
+  "channel": "shubham",
+  "views": "58M",
+  "title": "Cost of Living, The (Le coût de la vie)",
+  "time": "46 days ago",
+  "verified": true
+}, {
+  "id": 69,
+  "channel": "shubham",
+  "views": "30M",
+  "title": "Hidden Agenda",
+  "time": "42 days ago",
+  "verified": false
+}, {
+  "id": 70,
+  "channel": "shubham",
+  "views": "11M",
+  "title": "Beverly Hills Chihuahua",
+  "time": "93 days ago",
+  "verified": true
+}, {
+  "id": 71,
+  "channel": "shubham",
+  "views": "36M",
+  "title": "Inescapable",
+  "time": "9 days ago",
+  "verified": true
+}, {
+  "id": 72,
+  "channel": "shubham",
+  "views": "59M",
+  "title": "Midnight Express",
+  "time": "13 days ago",
+  "verified": false
+}, {
+  "id": 73,
+  "channel": "shubham",
+  "views": "67M",
+  "title": "Navigator: A Mediaeval Odyssey, The",
+  "time": "16 days ago",
+  "verified": false
+}, {
+  "id": 74,
+  "channel": "shubham",
+  "views": "92M",
+  "title": "Curse of the Golden Flower (Man cheng jin dai huang jin jia)",
+  "time": "29 days ago",
+  "verified": true
+}, {
+  "id": 75,
+  "channel": "shubham",
+  "views": "66M",
+  "title": "A.I. Artificial Intelligence",
+  "time": "54 days ago",
+  "verified": true
+}, {
+  "id": 76,
+  "channel": "shubham",
+  "views": "1M",
+  "title": "Beast Within, The",
+  "time": "86 days ago",
+  "verified": true
+}, {
+  "id": 77,
+  "channel": "shubham",
+  "views": "21M",
+  "title": "Shadow Riders, The",
+  "time": "70 days ago",
+  "verified": true
+}, {
+  "id": 78,
+  "channel": "shubham",
+  "views": "67M",
+  "title": "How to Eat Fried Worms",
+  "time": "43 days ago",
+  "verified": true
+}, {
+  "id": 79,
+  "channel": "shubham",
+  "views": "74M",
+  "title": "Game Plan, The",
+  "time": "51 days ago",
+  "verified": false
+}, {
+  "id": 80,
+  "channel": "shubham",
+  "views": "65M",
+  "title": "Step Into Liquid",
+  "time": "70 days ago",
+  "verified": false
+}, {
+  "id": 81,
+  "channel": "shubham",
+  "views": "27M",
+  "title": "Burt's Buzz",
+  "time": "22 days ago",
+  "verified": false
+}, {
+  "id": 82,
+  "channel": "shubham",
+  "views": "82M",
+  "title": "Thousand Words, A",
+  "time": "17 days ago",
+  "verified": false
+}, {
+  "id": 83,
+  "channel": "shubham",
+  "views": "3M",
+  "title": "Charlie Chan's Courage",
+  "time": "80 days ago",
+  "verified": false
+}, {
+  "id": 84,
+  "channel": "shubham",
+  "views": "71M",
+  "title": "Asylum",
+  "time": "15 days ago",
+  "verified": true
+}, {
+  "id": 85,
+  "channel": "shubham",
+  "views": "30M",
+  "title": "Decasia: The State of Decay",
+  "time": "2 days ago",
+  "verified": false
+}, {
+  "id": 86,
+  "channel": "shubham",
+  "views": "67M",
+  "title": "In Therapy (Divã)",
+  "time": "38 days ago",
+  "verified": true
+}, {
+  "id": 87,
+  "channel": "shubham",
+  "views": "52M",
+  "title": "Aloha Summer",
+  "time": "25 days ago",
+  "verified": true
+}, {
+  "id": 88,
+  "channel": "shubham",
+  "views": "45M",
+  "title": "Viva María!",
+  "time": "95 days ago",
+  "verified": false
+}, {
+  "id": 89,
+  "channel": "shubham",
+  "views": "54M",
+  "title": "Bread and Alley (Nan va Koutcheh)",
+  "time": "14 days ago",
+  "verified": true
+}, {
+  "id": 90,
+  "channel": "shubham",
+  "views": "30M",
+  "title": "Harold and Kumar Go to White Castle",
+  "time": "82 days ago",
+  "verified": true
+}, {
+  "id": 91,
+  "channel": "shubham",
+  "views": "83M",
+  "title": "The Case of the Lucky Legs",
+  "time": "25 days ago",
+  "verified": true
+}, {
+  "id": 92,
+  "channel": "shubham",
+  "views": "92M",
+  "title": "Heist",
+  "time": "31 days ago",
+  "verified": true
+}, {
+  "id": 93,
+  "channel": "shubham",
+  "views": "24M",
+  "title": "Ordinary Sinner",
+  "time": "99 days ago",
+  "verified": true
+}, {
+  "id": 94,
+  "channel": "shubham",
+  "views": "28M",
+  "title": "Great Passage, The (Fune wo amu)",
+  "time": "14 days ago",
+  "verified": true
+}, {
+  "id": 95,
+  "channel": "shubham",
+  "views": "15M",
+  "title": "The Cruel Sea",
+  "time": "25 days ago",
+  "verified": true
+}, {
+  "id": 96,
+  "channel": "shubham",
+  "views": "94M",
+  "title": "There Goes My Heart",
+  "time": "15 days ago",
+  "verified": false
+}, {
+  "id": 97,
+  "channel": "shubham",
+  "views": "86M",
+  "title": "Return of the Vampire, The",
+  "time": "19 days ago",
+  "verified": false
+}, {
+  "id": 98,
+  "channel": "shubham",
+  "views": "24M",
+  "title": "Bubba Ho-tep",
+  "time": "94 days ago",
+  "verified": true
+}, {
+  "id": 99,
+  "channel": "shubham",
+  "views": "9M",
+  "title": "Love and Other Catastrophes",
+  "time": "77 days ago",
+  "verified": true
+}, {
+  "id": 100,
+  "channel": "shubham",
+  "views": "94M",
+  "title": "Hollow Man",
+  "time": "17 days ago",
+  "verified": true
+}]
+
+export default videos
